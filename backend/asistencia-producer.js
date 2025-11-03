2d4dd308cc29eba039c75c9a196fc3ea9af4cac8
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-03 11:14:08 -0500

    Update asistencia-producer: create connection to RabbitMQ

--- backend/asistencia-producer.js
+++ backend/asistencia-producer.js
@@ -1,4 +1,5 @@
 const { Kafka } = require("kafkajs");
+const { amqp } = require("amqp");
 
 const kafka = new Kafka({
   clientId: "asistencia-producer",
@@ -8,8 +9,20 @@ const kafka = new Kafka({
 const producer = kafka.producer();
 const zones = ["salon1", "salon2", "salon3"];
 
+const RABBITMQ_URL = "amqp://192.168.80.1";
+
+async function conectRabbit() {
+  const connection = await amqp.connect(RABBITMQ_URL);
+  const channel = await connection.createChannel();
+  await channel.assertExchange(EXCHANGE, "direct", { durable: false });
+  console.log(`Conectado a RabbitMQ y exchange listo`);
+  return channel;
+}
+
 async function run() {
   await producer.connect();
+  rabbitChannel = await conectRabbit();
+
   setInterval(async () => {
     const zoneId = zones[Math.floor(Math.random() * zones.length)];
     const asistanceCount = Math.floor(Math.random() * 50) + 10;
@@ -21,6 +34,14 @@ async function run() {
     });
 
     console.log(`Enviado: ${JSON.stringify(msg)}`);
+
+    const mensajeRabbit = `Zona: ${zoneId} tiene ${asistanceCount} asistentes (${msg.timestamp})`;
+    rabbitChannel.publish(
+      EXCHANGE,
+      "asistencia.alerta",
+      Buffer.from(mensajeRabbit)
+    );
+    console.log(`RabbitMQ publicado: ${mensajeRabbit}`);
   }, 2000);
 }
 
