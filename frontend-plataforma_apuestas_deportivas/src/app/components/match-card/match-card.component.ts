855a8a0be2dec8bd8fbd37702c18eb30a4ad3554
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-17 05:51:36 -0500

    update match card

--- frontend-plataforma_apuestas_deportivas/src/app/components/match-card/match-card.component.ts
+++ frontend-plataforma_apuestas_deportivas/src/app/components/match-card/match-card.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Match } from '../../models/match.model';
 
 @Component({
   selector: 'app-match-card',
   imports: [],
   templateUrl: './match-card.component.html',
-  styleUrl: './match-card.component.scss'
+  styleUrl: './match-card.component.scss',
 })
 export class MatchCardComponent {
-
+  @Input() match!: Match;
 }
