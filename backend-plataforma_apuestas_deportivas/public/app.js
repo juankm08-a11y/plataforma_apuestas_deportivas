7d5a921ebdf4911ece262611345cee3f3843210c
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-17 16:42:27 -0500

    Update app.js

--- backend-plataforma_apuestas_deportivas/public/app.js
+++ backend-plataforma_apuestas_deportivas/public/app.js
@@ -1,30 +1,8 @@
-// async function loadMatches() {
-//   try {
-//     const res = await fetch("/matches");
-//     if (!res.ok) {
-//       throw new Error(`Error al cargar partidos: ${res.status}`);
-//     }
+const ws = new WebSocket("ws://localhost:8082");
 
-//     const matches = await res.json();
-//     const container = document.getElementById("matches");
-//     container.innerHTML = "";
-
-//     matches.forEach((m) => {
-//       const div = document.createElement("div");
-
-//       div.className = "match";
-//       div.innerHTML = `
-//                 <strong>${m.homeTeam}</strong> vs <strong>${
-//         m.awayTeam
-//       }</strong><br>
-//                 Inicio: ${new Date(m.startAt).toLocaleString()}
-
-//             `;
-//       container.appendChild(div);
-//     });
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-
-// loadMatches();
+ws.onmessage = (event) => {
+  const div = document.getElementById("alerts");
+  const p = document.createElement("p");
+  p.textContent = event.data;
+  div.appendChild(p);
+};
