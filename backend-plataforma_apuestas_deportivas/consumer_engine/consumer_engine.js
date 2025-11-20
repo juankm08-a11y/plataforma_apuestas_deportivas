3c7f3da37a51187b96db7d20b8030d1daee0b532
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-20 14:57:11 -0500

    update match generator

---
+++ backend-plataforma_apuestas_deportivas/consumer_engine/consumer_engine.js
@@ -0,0 +1,88 @@
+const {Kafka} = require("kafkajs")
+const amqp = require("amqp");
+
+const KAFKA_BROKER = "kafka:9092"
+const RABBITMQ_URL = "amqp://guest:guest@rabbitmq"
+
+const KAFKA_TOPIC = "match_events"
+
+const EXCHANGE = "betting_exchange"
+const ROUTING_KEY = "match.alert"
+
+async function start() {
+    console.log("Iniciando consumer_engine...")
+
+    const kafka = new Kafka({
+        cliend: "Consumer-engine",
+        brokers: [KAFKA_BROKER]
+    })
+
+    const consumer = kafka.consumer({groupId: "match-processing "})
+
+    await consumer.connect()
+    await consumer.subscribe({topic: KAFKA_TOPIC})
+
+    console.log("Conenctado a Kafka y escuchando:",KAFKA_TOPIC)
+
+    const rabbit = amqp.createConnection({url:RABBITMQ_URL})
+
+    rabbit.on("ready",() => {
+        rabbit.exchange(
+            EXCHANGE,
+            {type: "direct",durable:false},
+            (exchange) => {
+                console.log("RabbitMQ listo en consumer_engine")
+
+                consumer.run({
+                    eachMessage: async ({message}) => {
+                        try {
+                           const event = JSON.parse(message.value.toString())
+                           console.log("Evento kafka: recibido:",event)
+                           
+                           let alertMsg = {}
+
+                           switch (event.event_type) {
+                            case "MATCH_CREATED":
+                                alertMsg = {
+                                    event_type: "MATCH_CREATED",
+                                    match_id: event.match_id,
+                                    teams: event.teams,
+                                    tiemstamp: event.tiemstamp,
+                                    odds: {A:1.5,B:1.5}
+                                }
+                                case "MATCH_FINISHED":
+                                    alertMsg = {
+                                        event_type: "MATCH_FINISHED",
+                                        match_id: event.match_id,
+                                        teams: event.teams,
+                                        tiemstamp: event.tiemstamp,
+                                        result: event.result || "PENDING"
+                                    } 
+                                
+                                case "ODDS_UPDATED":
+                                    alertMsg = {
+                                        event_type: "ODDS_UPDATED",
+                                        match_id: event.match_id,
+                                        team: event.team,
+                                        odds: event.odds
+                                    }
+                                break
+
+                            
+                           }
+                           exchange.publish(ROUTING_KEY, JSON.stringify(alertMsg))
+                           console.log("---> reenviado a RabbitMQ:",alertMsg)
+
+                           
+                        } catch (error) {
+                            console.error("Error procesando mensaje Kafka:",err)
+                        }
+                    }
+                })
+            }
+        )
+    })
+
+}
+
+start()
